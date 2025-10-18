e1f93de5af70185fe39230f73a8d2c0b0c831ed0
AlejoLopeda <daniellopeda@gmail.com>
2025-10-18 05:41:14 -0500

    Cambios en el manejo del JWT token

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,9 +2,11 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const authMiddleware = require('../middlewares/authMiddleware');
 
 // Rutas de usuarios
 router.post('/', userController.guardarUsuario);
-router.get('/', userController.obtenerUsuarios);
+router.post('/login', userController.iniciarSesion);
+router.get('/', authMiddleware, userController.obtenerUsuarios);
 
 module.exports = router;
